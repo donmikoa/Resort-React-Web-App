56020a3023cf13c163ce0af934da6988e028e8e8
Michael <michael.iyaomolere@maxxpotential.com>
2020-01-02 12:04:56 -0500

    Making few updates on the context API.

--- src/components/FeaturedRooms.js
+++ src/components/FeaturedRooms.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
-import {RoomContext} from '../context';
+import { RoomContext } from '../context';
 
 export default class FeaturedRooms extends Component {
-    static contextType = RoomContext
+	static contextType = RoomContext;
 	render() {
-       
-        const {FeaturedRooms : rooms} = this.context
-        console.log(rooms);
-        
+		const { featuredRooms: rooms } = this.context;
+		console.log(rooms);
+
 		return <div>from featured rooms</div>;
 	}
 }
