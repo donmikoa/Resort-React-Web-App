6a2cfd0d2da45dbc2136042a5bc782671ed22f0a
Michael <michael.iyaomolere@maxxpotential.com>
2020-01-02 11:55:43 -0500

    Testing the Context API

--- src/components/FeaturedRooms.js
+++ src/components/FeaturedRooms.js
@@ -5,6 +5,8 @@ export default class FeaturedRooms extends Component {
     static contextType = RoomContext
 	render() {
        
+        const {FeaturedRooms : rooms} = this.context
+        console.log(rooms);
         
 		return <div>from featured rooms</div>;
 	}
