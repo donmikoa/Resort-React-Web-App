ed1d03afbcf805a6005228e6f1f0f747c2f9168a
Michael <michael.iyaomolere@maxxpotential.com>
2020-01-02 15:06:59 -0500

    Building the single room component

--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -1,7 +1,31 @@
 import React, { Component } from 'react';
+import defaultBcg from '../images/room-1.jpeg';
+import Hero from '../components/Hero';
+import Banner from '../components/Banner';
+import { Link } from 'react-router-dom';
+import { RoomContext } from '../context';
 
 export default class SingleRoom extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			slug: this.props.match.params.slug,
+			defaultBcg
+		};
+	}
+
+	static contextType = RoomContext;
+
 	render() {
+		const { getRoom } = this.context;
+		const room = getRoom(this.state.slug);
+		if (!room) {
+			return (
+				<div className='error'>
+					<h3>no such room could be found</h3>
+				</div>
+			);
+		}
 		return <div>Hello from single room page</div>;
 	}
 }
