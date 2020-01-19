da7f21116fcc4de6e421f27a0fecc33610625ac3
Michael <michael.iyaomolere@maxxpotential.com>
2020-01-19 16:47:19 -0500

    Created the Filter for Breakfast and Pets

--- src/context.js
+++ src/context.js
@@ -55,8 +55,7 @@ class RoomProvider extends Component {
 
 	handleChange = event => {
 		const target = event.target;
-
-		const value = event.type === 'checkbox' ? target.checked : target.value;
+		const value = target.type === 'checkbox' ? target.checked : target.value;
 		const name = event.target.name;
 		this.setState({ [name]: value }, this.filterRooms);
 	};
@@ -76,20 +75,32 @@ class RoomProvider extends Component {
 		let tempRooms = [...rooms];
 
 		//transform value
-		capacity =parseInt(capacity);
-		price =parseInt(price);
-
+		capacity = parseInt(capacity);
+		price = parseInt(price);
 
 		//filter by type
 		if (type !== 'all') {
 			tempRooms = tempRooms.filter(room => room.type === type);
 		}
 		//filter by capacity
-		if (capacity !==1){
-			tempRooms = tempRooms.filter(room => room.capacity >=capacity);
+		if (capacity !== 1) {
+			tempRooms = tempRooms.filter(room => room.capacity >= capacity);
 		}
-//filter by price
-tempRooms=tempRooms.filter(room => room.price<=price);
+		//filter by price
+		tempRooms = tempRooms.filter(room => room.price <= price);
+		//filter by size
+		tempRooms = tempRooms.filter(
+			room => room.size >= minSize && room.size <= maxSize
+		);
+		//filter by breakfast
+		if (breakfast) {
+			tempRooms = tempRooms.filter(room => room.breakfast == true);
+		}
+		//filter by pets
+		if (pets) {
+			tempRooms = tempRooms.filter(room => room.pets == true);
+		}
+
 		//change state
 		this.setState({ sortedRooms: tempRooms });
 	};
